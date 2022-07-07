ddffa978d5f3173f1011809798bc2f70555fd0dc
Joaquim Mulaza <joaquimmulazadev@gmail.com>
2022-07-07 07:48:22 +0100

    redesign Facebook

---
+++ script.js
@@ -0,0 +1,17 @@
+// let hover = window.document.getElementsByClassName('linkNav')[0];
+// let hover2 = window.document.getElementsByClassName('linkNav')[0]
+// hover.addEventListener("mouseenter", trocarElemento)
+// hover2.addEventListener("mouseout", trocar2)
+
+// function trocarElemento() {
+//     let hover = window.document.getElementsByClassName('linkNav')[0];
+//     // hover.innerText = 'Pagina Inicial'
+//     hover.innerHTML = '<i class="fas fa-house iconeNav"></i>'
+    
+// }
+
+// function trocar2() {
+//     let hover2 = window.document.getElementsByClassName('linkNav')[0]
+//     // hover2.innerHTML = '<i class="fas fa-house iconeNav"></i>'
+//     // hover2.innerText = 'Pagina Inicial'
+// }
